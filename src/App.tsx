0e93ea01a23eddec9ef5f411316776728cd21b09
Tin Dang <tin2dang@gmail.com>
2023-10-12 16:56:31 -0500

    installed tailwind and react router with assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='rounded overflow-hidden shadow-lg bg-white w-full'>
+      <div className="x-6 y-9">
+        <div className="bg-cover y-auto p-96 w-auto bg-[url('/src/assets/images/001fgo.jpg')]"></div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1 className='text-3xl font-bold underline'>
+        FGO Personality Test
+      </h1>
+      {/* <div className='box- h-25 w-20 p-4 border-4'>
+
+      </div> */}
+      <button className='bg-blue-500' onClick={() => console.log("clicked")}>Button</button>
+    </div>
   )
 }
 
